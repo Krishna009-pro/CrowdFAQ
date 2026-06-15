2dcc896613d05afe09cc2993d4659c8546d8b962
Krishna009-pro <kkp1882006@gmail.com>
2026-06-15 10:16:12 +0530

    removed the frontend code

--- frontend/src/components/layout/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const ProtectedRoute = ({ children }) => {
-  // Authentication bypass: directly render the target page component
-  return children;
-};
-
-export default ProtectedRoute;
