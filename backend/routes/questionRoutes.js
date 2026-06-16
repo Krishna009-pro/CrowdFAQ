da3a1d2eb37f225264e7a4696757629d7265d2e3
plum1010 <aadya.aadya.24@gmail.com>
2026-06-17 01:49:18 +0530

    feat: added question edit and close API's

--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -4,6 +4,8 @@ const {
   createQuestion,
   getQuestions,
   getQuestionById,
+  editQuestion,
+  deleteQuestion,
 } = require("../controllers/questionController");
 const { protect } = require("../middleware/authMiddleware");
 
@@ -12,5 +14,7 @@ const router = express.Router();
 router.post("/", protect, createQuestion);
 router.get("/", getQuestions);
 router.get("/:id", getQuestionById);
+router.patch("/:id", protect, editQuestion);
+router.delete("/:id", protect, deleteQuestion);
 
 module.exports = router;
